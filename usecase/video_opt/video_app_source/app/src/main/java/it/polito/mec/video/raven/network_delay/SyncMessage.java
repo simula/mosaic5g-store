4f62a60c7357bd50e0e35ba42293d5a9a61836df
eurecom <first.lastname@eurecom.fr>
2018-02-13 14:51:57 +0100

    Fix the video app/server folder

---
+++ usecase/video_opt/video_app_source/app/src/main/java/it/polito/mec/video/raven/network_delay/SyncMessage.java
@@ -0,0 +1,8 @@
+package it.polito.mec.video.raven.network_delay;
+
+public class SyncMessage {
+    public long timestamp_t0;
+    public long timestamp_t1;
+    public long timestamp_t2;
+    public long timestamp_t3;
+}
