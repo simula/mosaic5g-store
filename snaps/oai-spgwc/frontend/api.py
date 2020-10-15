3de98faa6604dea6dcf3dbfe40ceda95421c2197
Navid Nikaein <navid.nikaein@eurecom.fr>
2020-10-15 12:48:55 +0200

    Add openAPI and testing to oai-spgwc

--- snaps/oai-spgwc/frontend/api.py
+++ snaps/oai-spgwc/frontend/api.py
@@ -32,13 +32,7 @@ import argparse
 from werkzeug.utils import cached_property
 from werkzeug.datastructures import FileStorage
 
-snap_name = "oai-spgwc"
-
-api_host_default = '0.0.0.0'
-api_port_default = 5553
-
-api_manager_host_default = '0.0.0.0'
-api_manager_port_default = 6663
+from apivars import *
 
 SNAP="/snap/{}/current".format(snap_name)
 
